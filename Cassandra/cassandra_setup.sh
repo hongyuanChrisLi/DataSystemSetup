1c2349d0d3b9ab33633bb5cc6f2a5caf7ebe707b
Chris Li <kittokevin@gmail.com>
2017-02-03 15:29:15 -0800

    Install CentOS on VMWare and Cassandra

--- Cassandra/cassandra_setup.sh
+++ Cassandra/cassandra_setup.sh
@@ -1,1 +1,18 @@
-# Check Java Version (2017/02/03, Java 8) 
+# 1. verify ssh 
+# 2. Check Java Version (2017/02/03, Java 8) 
+java -version
+
+# 3. wget cassandra
+wget http://supergsego.com/apache/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz
+
+# 4. create cassandra directory
+mkdir /opt/cassandra
+
+# 5. set environment variable
+CASSANDRA_HOME=/opt/cassandra/apache-cassandra-3.9
+
+# 6. start cassandra
+${CASSANDRA_HOME}/bin/cassandra -f
+
+# 7. stop cassandra
+pkill -f CassandraDaemon
--- CentOS/os_setup.sh
+++ CentOS/os_setup.sh
@@ -1,6 +1,24 @@
-#Install CentOS (DVD ISO) on Virtual Machine
+# 1. Install Cento OSBoxes.org
+# 2. Update VM tools
+# 3. Enable multiuser mode:
+systemctl set-default multi-user.target
 
+# 4. Double Check
+systemctl get-default
 
-# 1. Software Selection
-# 	1.1 Infastructure Server + Java Platform
-# 2. Network Host name: Speicify hostname
+# 5. restart system
+# 6. Username: osboxes --> passowrd: osboxes.org
+# 7. Create new user
+
+adduser username
+passwd username
+usermod -aG wheel username
+
+# 8. Change hostname
+
+hostname server01
+logout
+
+# 9. Upldate 
+
+yum update
