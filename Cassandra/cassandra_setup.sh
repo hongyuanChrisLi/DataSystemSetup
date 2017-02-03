fa44e3beb856436f9f7a19baa2abe3014cf01993
Chris Li <kittokevin@gmail.com>
2017-02-03 10:48:39 -0800

    Centos server Installation on VMWare

---
+++ Cassandra/cassandra_setup.sh
@@ -0,0 +1,1 @@
+# Check Java Version (2017/02/03, Java 8) 
---
+++ CentOS/os_setup.sh
@@ -0,0 +1,6 @@
+#Install CentOS (DVD ISO) on Virtual Machine
+
+
+# 1. Software Selection
+# 	1.1 Infastructure Server + Java Platform
+# 2. Network Host name: Speicify hostname
