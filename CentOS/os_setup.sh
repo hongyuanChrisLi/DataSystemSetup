a5f84a08dc426543b8c5173d4a0baba85cf0ca7e
Chris Li <kittokevin@gmail.com>
2017-02-03 16:49:58 -0800

    Further configuration

--- CentOS/os_setup.sh
+++ CentOS/os_setup.sh
@@ -15,10 +15,22 @@ passwd username
 usermod -aG wheel username
 
 # 8. Change hostname
-
-hostname server01
-logout
+vi /etc/hostname
 
 # 9. Upldate 
-
 yum update
+
+# 10. Configure RSA key
+# generate a key pair with puttygen.exe (length: 1024 bits)
+# load the private key in the PuTTY profile
+# enter the public key in ~/.ssh/authorized_keys in one line (needs to start with ssh-rsa)
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+chown $USER:$USER ~/.ssh -R
+# change /etc/ssh/sshd_config so it contains AuthorizedKeysFile %h/.ssh/authorized_keys
+sudo service ssh restart
+
+# 11. Change Timezone
+cd  etc
+sudo mv /etc/localtime /etc/localtime.bak
+sudo ln -s ../usr/share/zoneinfo/US/Pacific /etc/localtime
