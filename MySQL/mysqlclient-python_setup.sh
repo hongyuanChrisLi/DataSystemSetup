089de3e7fb5c30bdbb65b88e51919b2e1e8e3302
Chris Li <kittokevin@gmail.com>
2017-02-10 17:17:06 -0800

    MySQL and Python

---
+++ MySQL/mysqlclient-python_setup.sh
@@ -0,0 +1,9 @@
+# Install mysqlclient-python
+
+
+# Download mysql connector python yum repository from
+# https://dev.mysql.com/downloads/connector/python/
+# install the package 
+rpm -Uvh mysql-connector-python-2.1.5-1.el7.x86_64.rpm
+
+
