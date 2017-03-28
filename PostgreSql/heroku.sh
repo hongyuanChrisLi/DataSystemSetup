ce062ca027571fdbaee098fef022ddbed31eed8d
Chris Li <kittokevin@gmail.com>
2017-03-27 17:11:03 -0700

    Update heroku.sh

--- PostgreSql/heroku.sh
+++ PostgreSql/heroku.sh
@@ -1,6 +1,7 @@
 # Heroku Postgres Setup
 # Create DB
 heroku addons:create heroku-postgresql:hobby-dev --as RET_DB
+heroku addons:create heroku-postgresql:hobby-basic --as RET_DB
 
 # Connect to database
 heroku pg:psql RET_DB_URL
